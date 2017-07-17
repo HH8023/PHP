7736cdb98acef134ce27e301218f0439f439b6bf
HH8023 <wp_0357@163.com>
2017-07-17 14:30:16 +0800

    强制的下载

---
+++ myphpstroage/714/demo4.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+	强制的下载
+*/
+
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>浏览器强制的下载</title>
+	</head>
+	<body>
+		<a href="./1.rar">点我啊~</a>
+		<a href="./3.gif">点我啊,兄弟~</a>
+		<a href="demo3.php">点我，哈哈</a>
+	</body>
+
+</html>
+
