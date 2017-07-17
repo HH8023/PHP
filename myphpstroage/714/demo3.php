c5e5318dd079fc30ae5cfaddf0cb2289a87bc03e
HH8023 <wp_0357@163.com>
2017-07-17 14:29:53 +0800

    默认浏览器下载

---
+++ myphpstroage/714/demo3.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+	默认的浏览器下载
+*/
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>浏览器默认的下载</title>
+	</head>
+	<body>
+		<a href="./1.rar">点我</a>
+		<a href="./3.gif">点我~</a>
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
